01516428fbf2d5140dd1bae5c81783395478e2e9
amirrezam <amirrezamozayani@gmail.com>
2018-02-02 12:18:22 +0330

    final

--- main.py
+++ main.py
@@ -11,9 +11,29 @@ import numpy as np
 from sklearn.model_selection import cross_val_score
 
 # from hazm import *
+#
+# text = "اصلاح نويسه ها و استفاده کتاب‌ها از نیم‌فاصله پردازش را آسان مي رود"
+#
+# normalizer = Normalizer()
+# stemmer = Stemmer()
+# lemmatizer = Lemmatizer()
+# print(normalizer.normalize(text))
+# text2 = normalizer.normalize(text)
+# print(word_tokenize(text))
+#
+# arr = word_tokenize(text)
+# arr2 = [stemmer.stem(x) for x in arr]
+# arr3 = [lemmatizer.lemmatize(x) for x in arr]
+# print(arr2)
+# print(arr3)
+# print(stemmer.stem(text))
+# final=" ".join(arr2)
+# print(final)
+#
+# print(lemmatizer.lemmatize("کتاب‌ها"))
 
 
-data = load_files('./data', encoding='utf-8')
+# data = load_files('./data', encoding='utf-8')
 
 # count_vect = CountVectorizer()
 # X_train_counts = count_vect.fit_transform(data.data)
@@ -26,25 +46,22 @@ data = load_files('./data', encoding='utf-8')
 # print(clf2)
 # print(clf)
 # print(X_train_tfidf)
-print(data)
+# print(data)
 
-text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), (
-'clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5, tol=None))])
-text_clf.fit(data.data, data.target)
-joblib.dump(text_clf, "model.txt")
 
-predicted = text_clf.predict(data.data)
 
-acc = np.mean(predicted == data.target)
-
-scores = cross_val_score(text_clf, data.data, data.target, cv=2)
-
-print(np.array([2, 3]).mean())
-print(acc)
-print(metrics.classification_report(data.target, predicted, target_names=data.target_names))
-print(metrics.confusion_matrix(data.target, predicted))
-
-print(text_clf)
+# text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), (
+# 'clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5, tol=None))])
+# text_clf.fit(data.data, data.target)
+# joblib.dump(text_clf, "model.txt")
+# predicted = text_clf.predict(data.data)
+# acc = np.mean(predicted == data.target)
+# scores = cross_val_score(text_clf, data.data, data.target, cv=2)
+# print(np.array([2, 3]).mean())
+# print(acc)
+# print(metrics.classification_report(data.target, predicted, target_names=data.target_names))
+# print(metrics.confusion_matrix(data.target, predicted))
+# print(text_clf)
 
 
 
--- modules.py
+++ modules.py
@@ -4,13 +4,28 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.utils import Bunch
 
+# from hazm import *
+
 
 def normalize_input(train_content_file_address, train_label_file_address):
     content_file = open(train_content_file_address, "r", encoding="utf8")
     label_file = open(train_label_file_address, "r")
     data = Bunch(data=[], target=[])
+    # normalizer = Normalizer()
+    # stemmer = Stemmer()
+    # lemmatizer = Lemmatizer()
 
     for line in content_file:
+        # print(line)
+        # text = normalizer.normalize(line)
+        # arr = word_tokenize(text)
+        # arr_stemmize = [stemmer.stem(x) for x in arr]
+        # arr_lemmatize = [lemmatizer.lemmatize(x) for x in arr]
+        # final_lemmatize = " ".join(arr_lemmatize)
+        # final_stemmize = " ".join(arr_stemmize)
+        # print(final_stemmize)
+        # print(final_lemmatize)
+        # sys.exit()
         data.data.append(line)
 
     for line in label_file:
@@ -21,4 +36,4 @@ def normalize_input(train_content_file_address, train_label_file_address):
 
 def create_model():
     return Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), (
-    'clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5, tol=None))])
+    'clf', SGDClassifier(loss='hinge', penalty='l1', alpha= 1e-5, random_state=42, max_iter=5, tol=None))])
